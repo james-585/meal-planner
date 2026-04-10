5d5b7bf4dda1267269e594a237cdc8d9b409cfe1
james-585 <43157117+james-585@users.noreply.github.com>
2026-04-10 11:21:11 +1000

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+import express from 'express';
+import fetch from 'node-fetch';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Welcome to the Meal Planner API!');
+    res.sendFile('index.html', { root: 'public' });
 });
 
 app.listen(PORT, () => {
